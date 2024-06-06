452a2ecff28d2a4ad6a043e0884726d1f983a019
Fernando Mateo Valencia Gómez <fmvalencia@uninorte.edu.co>
2024-06-06 10:30:22 -0500

    set bug fixed

--- src/components/CacheConfigForm.js
+++ src/components/CacheConfigForm.js
@@ -224,7 +224,7 @@ const CacheConfigForm = ({ cache, setCache, memory, setMemory, setSegmentation }
                             className="mt-1 p-1 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
                         />
                     </div>
-                    {location.pathname === '/set-associative' && (
+                    {location.pathname === '/set' && (
                     <div>
                         <label className="block text-sm font-medium text-gray-700">
                           Number of Sets:
